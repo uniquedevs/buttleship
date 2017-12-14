a8569cd557beaede70a997417b9bb92ac5a0bf8c
max <maxim.evdokimov@loudclear.com.au>
2017-12-14 09:09:21 +0200

    fire logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import {shipGenerator} from './ShipGenerator';
-import {BOARD_SIZE} from './config';
+import {BOARD_SIZE, STATE_MAP} from './config';
 import Dot from './Dot';
 
 
@@ -18,21 +18,21 @@ class App extends Component {
             .placeShip(1);
 
         this.ships = shipGenerator.ships;
-
-        console.log(this.generateBoard());
+        this.setState({board : this.generateBoard()});
     }
 
     generateBoard() {
-        let rows = [],
-            ships_cells = [].concat(...this.ships);
-        console.log(ships_cells);
+        let rows = [];
         for (let i = 0; i < BOARD_SIZE; i++) {
             let cells = [];
             for (let j = 0; j < BOARD_SIZE; j++) {
                 let cell = 0;
-                for (let s = 0; s < ships_cells.length; s++) {
-                    if (ships_cells[s][0] === i && ships_cells[s][1] === j) {
-                        cell = 1;
+                for (let s = 0; s < this.ships.length; s++) {
+                    const ship = this.ships[s];
+                    for (let c = 0; c < ship.length; c++) {
+                        if (ship[c][0] === i && ship[c][1] === j) {
+                            cell = s + 1;
+                        }
                     }
                 }
                 cells.push(cell);
@@ -42,18 +42,42 @@ class App extends Component {
         return rows;
     }
 
-    render() {
-        let rows = [];
-        for (let i = 0; i < BOARD_SIZE; i++) {
-            let cell = [];
-            for (let j = 0; j < BOARD_SIZE; j++) {
-                cell.push(<Dot key={j}/>);
-            }
-            rows.push(<div key={i} className="row">{cell}</div>);
+    clickFire() {
+        const   row = Math.floor(Math.random() * BOARD_SIZE),
+                col = Math.floor(Math.random() * BOARD_SIZE),
+                board = this.state.board
+        ;
+        let new_board = [...board],
+            cell = new_board[row][col]
+        ;
+        if (cell === 0) {
+            new_board[row][col] = BOARD_SIZE * BOARD_SIZE + 2;
+        } else if (cell === BOARD_SIZE * BOARD_SIZE + 2 || cell === BOARD_SIZE * BOARD_SIZE + 1) {
+            return this.clickFire();
+        } else if (cell > 0 && cell <= this.ships.length) {
+            new_board[row][col] = BOARD_SIZE * BOARD_SIZE + 1;
         }
+
+        this.setState({board : new_board});
+    }
+
+    hit() {
+
+    }
+
+    render() {
+        let board = this.state.board;
+
         return (
-            <div className="App">
-                {rows}
+            <div>
+                <div className="App">
+                    {board.map( (row, r_idx) => {
+                        return (<div className="row" key={r_idx}>
+                        { row.map( (cell, c_idx) => (<Dot key={c_idx} status={STATE_MAP[cell]} val={cell}/>)) }
+                        </div>)
+                    })}
+                </div>
+                <input type="button" value="Fire!" onClick={this.clickFire.bind(this)}/>
             </div>
         );
     }
--- src/config.js
+++ src/config.js
@@ -1,2 +1,7 @@
 export const BOARD_SIZE = 10;
 export const L_SHIP_ASPECT_RATIO = 0.5;
+export const STATE_MAP = {
+    0: 'empty',
+    [BOARD_SIZE * BOARD_SIZE + 1] : 'sink',
+    [BOARD_SIZE * BOARD_SIZE + 2] : 'missed'
+};
